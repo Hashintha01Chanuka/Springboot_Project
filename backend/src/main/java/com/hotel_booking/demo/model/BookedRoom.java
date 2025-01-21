768859c9815e122337021055e500a92344eb7743
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2025-01-21 08:31:24 +0530

    Add BookedRoom model

--- backend/src/main/java/com/hotel_booking/demo/model/BookedRoom.java
+++ backend/src/main/java/com/hotel_booking/demo/model/BookedRoom.java
@@ -1,5 +1,24 @@
 package com.hotel_booking.demo.model;
 
+import java.time.LocalDate;
+
 public class BookedRoom {
+        private Long bookingId;
+
+        private LocalDate checkInDate;
+
+        private LocalDate checkOutDate;
+        
+        private String guestFullName;
+
+        private String guestEmail;
+
+        private int NumOfAdults;
+
+        private int NumOfChildren;
+
+        private int totalNumOfGuest;
+
+
 
 }
