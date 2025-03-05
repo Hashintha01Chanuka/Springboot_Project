9646fd9472766f888fff44ac464e94164f416238
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2025-03-05 22:21:56 +0530

    Modify BookedRoom file

--- backend/src/main/java/com/hotel_booking/demo/model/BookedRoom.java
+++ backend/src/main/java/com/hotel_booking/demo/model/BookedRoom.java
@@ -44,6 +44,7 @@ public class BookedRoom {
         @Column(name = "confirmation_code")
         private String bookingConfirmationCode;
 
+        @ManyToOne(fetch = FetchType.LAZY)
         @JoinColumn(name = "room_id")
         private Room room;
 
