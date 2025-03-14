19eee70004fe71e4f797fffcbf0e54c6ed63a72b
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2025-03-14 22:44:03 +0530

    Modify the RoomResponse file

--- backend/src/main/java/com/hotel_booking/demo/response/RoomResponse.java
+++ backend/src/main/java/com/hotel_booking/demo/response/RoomResponse.java
@@ -1,4 +1,15 @@
 package com.hotel_booking.demo.response;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class RoomResponse {
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked;
+    private String photo;
+
+
+
 }
