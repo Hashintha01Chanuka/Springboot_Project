07d7896c4071ba4bbc569d1fbc38a82731907caa
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2025-03-17 17:24:06 +0530

    Update BookingResponse file

--- backend/src/main/java/com/hotel_booking/demo/response/BookingResponse.java
+++ backend/src/main/java/com/hotel_booking/demo/response/BookingResponse.java
@@ -1,4 +1,45 @@
 package com.hotel_booking.demo.response;
 
+import com.hotel_booking.demo.model.Room;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class BookingResponse {
+    private Long id;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    private String guestName;
+
+    private String guestEmail;
+
+    private int NumOfAdults;
+
+    private int NumOfChildren;
+
+    private int totalNumOfGuest;
+
+    private String bookingConfirmationCode;
+
+    private RoomResponse room;
+
+    public BookingResponse(Long id, LocalDate checkInDate, LocalDate checkOutDate, String bookingConfirmationCode) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
--- backend/src/main/java/com/hotel_booking/demo/response/RoomResponse.java
+++ backend/src/main/java/com/hotel_booking/demo/response/RoomResponse.java
@@ -2,9 +2,9 @@ package com.hotel_booking.demo.response;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
 
 import java.math.BigDecimal;
-import java.util.Base64;
 import java.util.List;
 
 @Data
@@ -33,7 +33,7 @@ public class RoomResponse {
         this.roomType = roomType;
         this.roomPrice = roomPrice;
         this.isBooked = isBooked;
-        this.photo = photoBytes != null ? Base64.encodeBase64String(photoBytes) : null;
+        this.photo = photoBytes != null ? Base64.encodeBase64String(photoBytes): null;
         this.bookings = bookings;
     }
 }
